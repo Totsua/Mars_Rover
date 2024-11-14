32236b0231a1d9a7a32ae833f1b352f97ee3c425
Shamar <Shamar098@gmail.com>
2024-11-14 19:45:45 +0000

    feat: add InputParser methods implementation

--- src/main/java/org/example/model/parser/InputParser.java
+++ src/main/java/org/example/model/parser/InputParser.java
@@ -5,21 +5,100 @@ import org.example.model.Instruction;
 import org.example.model.PlateauSize;
 import org.example.model.Position;
 
+import java.util.ArrayList;
+
 public class InputParser {
 
-  CompassDirections directionParser(String input){
-      return null;
-  }
+    CompassDirections directionParser(String input) {
+        switch (input) {
+            case ("N"), ("n"):
+                return CompassDirections.N;
+            case ("E"), ("e"):
+                return CompassDirections.E;
+            case ("W"), ("w"):
+                return CompassDirections.W;
+            case ("S"), ("s"):
+                return CompassDirections.S;
+            case null:
+                return null;
+            default:
+                // maybe throw error?*/
+        }
 
-  PlateauSize plateauSizeParser(String input){
-      return null;
-  }
-
-    Position positionParser(String input){
         return null;
     }
-  Instruction[] instructionParser(String input){
-        return null;
+
+    PlateauSize plateauSizeParser(String input) {
+        if (input == null || input.trim().split(" ").length != 2) {
+            return null;
+        } else {
+            try {
+                String[] splitString = input.split(" ");
+                int x = Integer.parseInt(splitString[0]) > 0
+                        ? Integer.parseInt(splitString[0]) : -Integer.parseInt(splitString[0]);
+                int y = Integer.parseInt(splitString[1]) > 0
+                        ? Integer.parseInt(splitString[1]) : -Integer.parseInt(splitString[1]);
+                if (x == 0 && y == 0) {
+                    return null;
+                }
+
+                return new PlateauSize(x, y);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+    }
+
+    Position positionParser(String input) {
+        if(input == null || input.isBlank()){
+            return null;
+        }
+
+        String[] inputSplit = input.split(" ");
+        try{
+            int x = Integer.parseInt(inputSplit[0]);
+            int y = Integer.parseInt(inputSplit[1]);
+            CompassDirections direction = directionParser(inputSplit[2]);
+            if(direction == null){
+                return null;
+            }
+            return new Position(x,y,direction);
+        }catch (NumberFormatException e ){
+            return null;
+        }
+
+    }
+
+    Instruction[] instructionParser(String input) {
+
+        if (input == null || input.isBlank()) {
+            return null;
+        }
+        ArrayList<Instruction> instructions = new ArrayList<>();
+
+        for (Character c : input.toCharArray()) {
+            switch (c) {
+                case ('L'), ('l'):
+                    instructions.add(Instruction.L);
+                    break;
+                case ('R'), ('r'):
+                    instructions.add(Instruction.R);
+                    break;
+                case ('M'), ('m'):
+                    instructions.add(Instruction.M);
+                    break;
+                default:
+                    // maybe throw error
+            }
+        }
+
+        Instruction[] instructionArray = new Instruction[instructions.size()];
+        for (int i = 0; i < instructions.size(); i++) {
+            instructionArray[i] = instructions.get(i);
+        }
+
+
+        return instructionArray.length > 0 ? instructionArray : null;
     }
 }
 
