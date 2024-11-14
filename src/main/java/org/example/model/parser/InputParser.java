d5b8d6d87205320b9d7d0a7e63faebdb1f4afdbe
Shamar <Shamar098@gmail.com>
2024-11-14 15:47:48 +0000

    feat: add InputParser class

---
+++ src/main/java/org/example/model/parser/InputParser.java
@@ -0,0 +1,20 @@
+package org.example.model.parser;
+
+import org.example.model.CompassDirections;
+import org.example.model.Instruction;
+import org.example.model.PlateauSize;
+
+public class InputParser {
+
+  CompassDirections directionParser(String input){
+      return null;
+  }
+
+  PlateauSize plateauSizeParser(String input){
+      return null;
+  }
+  Instruction instructionParser(String input){
+      return null;
+  }
+}
+
