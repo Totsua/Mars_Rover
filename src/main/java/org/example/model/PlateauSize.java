6ad878b1483c884a88b640931b41a12be1ac74a9
Shamar <Shamar098@gmail.com>
2024-11-14 23:51:56 +0000

    feat: PlateauSize changes negative inputs to positive

--- src/main/java/org/example/model/PlateauSize.java
+++ src/main/java/org/example/model/PlateauSize.java
@@ -14,9 +14,9 @@ public class PlateauSize {
         if(upperRightGridCoordinate == null || Arrays.equals(upperRightGridCoordinate, new int[]{0, 0})){
             plateauGrid = null;
         }else {
-            this.x = x;
-            this.y = y;
-            plateauGrid = new int[x+1][y+1];
+            this.x = x > 0 ? x : -x;
+            this.y = y > 0 ? y : -y;
+            plateauGrid = new int[this.x+1][this.y+1];
         }
 
     }
