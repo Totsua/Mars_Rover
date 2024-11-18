a2723d9b713d0cd27aff1b3d8f50736fdd6e04c4
Shamar <Shamar098@gmail.com>
2024-11-18 11:38:26 +0000

    feat: add serviceLayerimpl implementation to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.service.ServiceLayerImpl;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String input = "5 5";
+        String input2 ="1 2 N" ;
+        String input3 ="LMLMLMLMMl";
+
+
+        ServiceLayerImpl serviceLayer = new ServiceLayerImpl();
+
+        System.out.println(serviceLayer.createPlateau(input));
+        System.out.println(serviceLayer.createRover(input2));
+        System.out.println(serviceLayer.instructRover(input3));
+        System.out.println(serviceLayer.getRover().getPosition());
+
+
+
     }
 }
